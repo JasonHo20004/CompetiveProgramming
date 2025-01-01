899b251b61c6cbf06ec04d0eff82dd1ffb537a26
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2025-01-01 19:52:11 +0700

    Add solution for LeetCode problem 1636: Sort Array by Increasing Frequency

---
+++ LeetCode Solutions/Easy/1636. Sort Array by Increasing Frequency/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿public:
+    vector<int> frequencySort(vector<int>& nums) {
+    unordered_map<int, int> freq;
+    for (int num : nums) {
+        freq[num]++;
+    }
+
+    // Using a max heap to sort by frequency descending
+    auto comp = [](const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second || (a.second == b.second && a.first < b.first);
+    };
+    priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> pq(comp);
+
+    for (auto& it : freq) {
+        pq.push(it);
+    }
+
+    vector<int> result;
+    while (!pq.empty()) {
+        auto [num, count] = pq.top();
+        pq.pop();
+        while (count--) {
+            result.push_back(num);
+        }
+    }
+    return result;
+}
+
+};
