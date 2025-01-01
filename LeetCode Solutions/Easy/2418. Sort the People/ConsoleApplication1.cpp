43fa7eceefa780f9f7bd35fac2493f868747ff0a
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2025-01-01 19:52:27 +0700

    Add solution for LeetCode problem 2418: Sort the People

---
+++ LeetCode Solutions/Easy/2418. Sort the People/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿class Solution {
+public:
+    int partition(vector<string>& names, vector<int>& heights, int low, int high) {
+        int pivot = heights[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (heights[j] >= pivot) {
+                i++;
+                swap(heights[i], heights[j]);
+                swap(names[i], names[j]);
+            }
+        }
+        swap(heights[i + 1], heights[high]);
+        swap(names[i + 1], names[high]);
+        return i + 1;
+    }
+
+    void quicksort(vector<string>& names, vector<int>& heights, int low, int high) {
+        if (low < high) {
+            int pi = partition(names, heights, low, high);
+
+            quicksort(names, heights, low, pi - 1);
+            quicksort(names, heights, pi + 1, high);
+        }
+    }
+
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        quicksort(names, heights, 0, heights.size() - 1);
+        return names;
+    }
+};
