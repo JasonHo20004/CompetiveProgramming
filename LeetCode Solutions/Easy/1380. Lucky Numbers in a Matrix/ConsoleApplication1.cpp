eafbf33af740549b0ee33f79b67f77fc45af21e8
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2025-01-01 09:41:55 +0700

    Add solution for LeetCode problem 1380: Lucky Numbers in a Matrix.

---
+++ LeetCode Solutions/Easy/1380. Lucky Numbers in a Matrix/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿class Solution {
+public:
+    vector<int> luckyNumbers(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> minInRows(m, INT_MAX);
+        unordered_map<int, int> rowtoColMap;
+        for (int i = 0; i < m; ++i) {
+            int minRowVal = INT_MAX;
+            int minColIndex = -1;
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] < minRowVal) {
+                    minRowVal = matrix[i][j];
+                    minColIndex = j;
+                }
+            }
+            minInRows[i] = minRowVal;
+            rowtoColMap[i] = minColIndex;
+        }
+        vector<int> luckyNumbers;
+        for (int i = 0; i < m; ++i) {
+            int minRowVal = minInRows[i];
+            int colIndex = rowtoColMap[i];
+            bool isLucky = true;
+            for (int j = 0; j < m; ++j) {
+                if (matrix[j][colIndex] > minRowVal) {
+                    isLucky = false;
+                    break;
+                }
+            }
+            if (isLucky) {
+                luckyNumbers.push_back(minRowVal);
+            }
+        }
+        return luckyNumbers;
+    }
+};
