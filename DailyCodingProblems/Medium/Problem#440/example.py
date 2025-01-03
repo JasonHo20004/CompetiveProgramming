52e674cdb548ad461d266a928a259fea60b20f65
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2025-01-03 11:02:39 +0700

    Added a generic function `print_list` to print items in a list, supporting different data types

--- DailyCodingProblems/Medium/Problem#440/example.py
+++ DailyCodingProblems/Medium/Problem#440/example.py
@@ -22,3 +22,15 @@ printValue(12)
 printValue(3.44)
 printValue("ABC")
 printValue([1, 2, 3]) #Unsupported type
+
+from typing import TypeVar, List
+
+T = TypeVar('T')
+def print_list(items: list[T]) -> None:
+    for item in items:
+        print(item, end=" ")
+    print()
+
+print_list([1, 2, 3])
+print_list([1.1, 2.2, 3.3])
+print_list(["A", "B", "C"])
