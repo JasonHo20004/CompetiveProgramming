9125bc8c1c23fda6c7a7dac8476a4df350bb04c7
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2025-01-07 11:50:11 +0700

    Optimize string matching by sorting words by length

--- LeetCode Solutions/Easy/1408. String Matching in an Array/solution.cpp
+++ LeetCode Solutions/Easy/1408. String Matching in an Array/solution.cpp
@@ -2,20 +2,47 @@
 #include <string>
 
 using namespace std;
+//#1 approach:
+
+// class Solution {
+// public:
+//     vector<string> stringMatching(vector<string>& words) {
+//         int n = words.size();
+//         vector<string> result;
+//         for(int i = 0; i < n; ++i){
+//             for(int j = 0; j < n; ++j){
+//                 if(i != j && words[j].find(words[i]) != string::npos){
+//                     result.push_back(words[i]);
+//                     break;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+//#2 approach:
 
 class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
-        int n = words.size();
+        // Sort words by length
+        sort(words.begin(), words.end(), [](const string& a, const string& b) {
+            return a.size() < b.size();
+        });
+        
         vector<string> result;
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < n; ++j){
-                if(i != j && words[j].find(words[i]) != string::npos){
+        int n = words.size();
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (words[j].find(words[i]) != string::npos) {
                     result.push_back(words[i]);
                     break;
                 }
             }
         }
+        
         return result;
     }
 };
