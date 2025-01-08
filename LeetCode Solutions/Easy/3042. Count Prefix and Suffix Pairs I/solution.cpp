66f9b346659361bbc68255e446b3976e67d6699c
Ho Thanh Dat <125708929+JasonHo20004@users.noreply.github.com>
2025-01-08 11:00:45 +0700

    Refactor isPrefixandSuffix to improve readability and efficiency

--- LeetCode Solutions/Easy/3042. Count Prefix and Suffix Pairs I/solution.cpp
+++ LeetCode Solutions/Easy/3042. Count Prefix and Suffix Pairs I/solution.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
 
-    bool isPrefixandSuffix(const string& str1, const string& str2){
-        return str2.substr(0, str1.length()) == str1 && 
-           str2.substr(str2.length() - str1.length(), str1.length()) == str1;
+    // bool isPrefixandSuffix(const string& str1, const string& str2){
+    //     return str2.substr(0, str1.length()) == str1 && 
+    //        str2.substr(str2.length() - str1.length(), str1.length()) == str1;
+    // }
+
+    bool isPrefixandSuffix(const string& str1, const string& str2) {
+        if (str1.length() > str2.length()) return false;
+
+        // Check prefix
+        for (size_t i = 0; i < str1.length(); ++i) {
+            if (str1[i] != str2[i]) return false;
+        }
+
+        // Check suffix
+        size_t offset = str2.length() - str1.length();
+        for (size_t i = 0; i < str1.length(); ++i) {
+            if (str1[i] != str2[offset + i]) return false;
+        }
+
+        return true;
     }
+
     int countPrefixSuffixPairs(vector<string>& words) {
         int count =  0;
         for(int i = 0; i < words.size(); ++i){
